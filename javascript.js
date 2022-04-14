d26ef2fd0d03216633a21346e04577a17f6e7ef4
edouard <edouard.blais@hotmail.com>
2022-04-14 14:50:26 -0400

    displays operands

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+const operandButton = document.getElementsByClassName('operand');
+const operatorButton = document.getElementsByClassName('operator');
+const clearButton = document.getElementById('clear');
+const deleteButton = document.getElementById('delete');
+const decimalButton = document.getElementById('decimal');
+const equalButton = document.getElementById('equal');
+const screenButton = document.getElementById('screen');
+
 let add = (a, b) => a+b;
 
 let substract = (a, b) => a-b;
@@ -5,6 +13,11 @@ let substract = (a, b) => a-b;
 let multiply = (a, b) => a*b;
 
 let divide = (a, b) => a/b;
+
+for (let i=0; i<operandButton.length; i++) {
+    operandButton[i].addEventListener('click', () => append(operandButton[i].textContent));
+};
+
 
 function operate(a, b, operator) {
     if (operator==='+') {
@@ -17,3 +30,11 @@ function operate(a, b, operator) {
     return divide(a, b);
 };
 };
+
+function append(choice) {
+    screenButton.textContent += choice;
+};
+
+function resetScreen() {
+    screenButton.textContent = '';
+}
