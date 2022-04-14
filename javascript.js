d45c50e224701dd509da8e5482d2281b67538ccc
edouard <edouard.blais@hotmail.com>
2022-04-14 17:17:05 -0400

    logic almost done

--- javascript.js
+++ javascript.js
@@ -6,6 +6,10 @@ const decimalButton = document.getElementById('decimal');
 const equalButton = document.getElementById('equal');
 const screenButton = document.getElementById('screen');
 
+let firstOperand = '';
+let secondOperand = '';
+let operator = '';
+
 let add = (a, b) => a+b;
 
 let substract = (a, b) => a-b;
@@ -18,17 +22,56 @@ for (let i=0; i<operandButton.length; i++) {
     operandButton[i].addEventListener('click', () => append(operandButton[i].textContent));
 };
 
+for (let i=0; i<operatorButton.length; i++) {
+    operatorButton[i].addEventListener('click', () => evaluate(operatorButton[i].textContent));
+};
+
+clearButton.addEventListener('click', () => {
+    resetScreen();
+    firstOperand='';
+    secondOperand='';
+    operator=''
+});
+
+deleteButton.addEventListener('click', () => {
+    let oldText = screenButton.textContent;
+    let newText = oldText.slice(0, -1);
+    screenButton.textContent = newText;
+});
+
+function evaluate(x) {
+    if (firstOperand==='') {
+        firstOperand = screenButton.textContent;
+        operator = x;
+        resetScreen();
+        return;
+        };
+    secondOperand = screenButton.textContent;
+    if (operator==='/' && secondOperand==='0') {
+        screenButton.textContent='Error:Division by 0';
+        return;
+    };
+    let result = operate(parseInt(firstOperand), parseInt(secondOperand), operator);
+    firstOperand = result;
+    if (x==='=') {
+        screenButton.textContent = (Math.round(firstOperand*100))/100;
+        return;
+        };
+    operator = x;
+    resetScreen();
+    return;
+};
 
 function operate(a, b, operator) {
     if (operator==='+') {
         return add(a,b);
-} else if (operator==='-') {
-    return substract(a, b);
-} else if (operator==='*') {
-    return multiply(a, b);
-} else if (operator==='/') {
-    return divide(a, b);
-};
+    }  else if (operator==='-') {
+        return substract(a, b);
+    } else if (operator==='*') {
+        return multiply(a, b);
+    } else if (operator==='/') {
+        return divide(a, b);
+    };
 };
 
 function append(choice) {
@@ -37,4 +80,4 @@ function append(choice) {
 
 function resetScreen() {
     screenButton.textContent = '';
-}
+};
